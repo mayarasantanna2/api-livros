d920c9d0983fe853aae012638270fce48b35cf70
mayarasantanna2 <mayaraoli.anna@gmail.com>
2026-09-24 07:32:55 -0300

    feat: permite acesso do frontend

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -20,7 +20,9 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `biblioteca_db`
 --
-
+CREATE DATABASE biblioteca_db
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
 -- --------------------------------------------------------
 
 --
